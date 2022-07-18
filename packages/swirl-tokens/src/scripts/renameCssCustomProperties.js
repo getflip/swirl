31579b1f34f6a8b0649b168cf678275beae88a72
Adam Gniady <neunfuenf@gmail.com>
2022-07-18 17:53:17 +0200

    cleaned rename module

--- packages/swirl-tokens/src/scripts/renameCssCustomProperties.js
+++ packages/swirl-tokens/src/scripts/renameCssCustomProperties.js
@@ -1,29 +1,9 @@
 const fs = require('fs');
 
-/**
- * Read out CSS File as string
- * @param {string} cssFilePath 
- * @returns string
- */
-function readCss(cssFilePath) {
-    const css = fs.readFileSync(cssFilePath, 'utf8');
-    return css;
-}
-
-/**
- * Write transformed CSS to file
- * @param {string} cssPath 
- * @param {string} transformedCss 
- */
-function writeCss(cssPath ,transformedCss) {
-    fs.writeFileSync(cssPath, transformedCss);
-}
-
 
 module.exports = function(cssFilePath) {
     return new Promise((resolve) => {
-        console.log("starting renaming in... ", cssFilePath);
-        const css = readCss(cssFilePath);
+        const css = fs.readFileSync(cssFilePath, 'utf8');
         const newCss = css.split(/\r?\n/).map(line =>  {
             if(line.includes('--')) {
                 line = `  --s-${line.substring(4, line.length)}`
@@ -34,8 +14,7 @@ module.exports = function(cssFilePath) {
             return line;
         });
         const transformedCss = newCss.join('\n');
-        writeCss(cssFilePath, transformedCss);
-        console.log("done renaming in ",  cssFilePath);
+        fs.writeFileSync(cssFilePath, transformedCss);
         resolve();
     })
 }
