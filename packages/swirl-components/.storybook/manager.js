bd1389df80f056975ceb7fc7a5c3a5dca359bfdf
Philipp Siekmann <philipp.siekmann@flipapp.de>
2022-08-09 17:11:52 +0200

    feat: storybook theming

---
+++ packages/swirl-components/.storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from "@storybook/addons";
+import swirlTheme from "./theme";
+
+addons.setConfig({
+  theme: swirlTheme,
+});
--- packages/swirl-components/.storybook/preview.js
+++ packages/swirl-components/.storybook/preview.js
@@ -1,5 +1,6 @@
 import { defineCustomElements } from "../loader";
 import { setStencilDocJson } from "@pxtrn/storybook-addon-docs-stencil";
+import { themes } from "@storybook/theming";
 
 import docJson from "../components.json";
 
@@ -16,6 +17,31 @@ export const parameters = {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
+    },
+  },
+  docs: {
+    theme: {
+      base: "light",
+
+      appBg: "white",
+      appBorderColor: "rgba(0, 6, 29, 0.09)",
+      appBorderRadius: 8,
+      appContentBg: "white",
+      barBg: "white",
+      barSelectedColor: "rgba(0, 0, 0, 1)",
+      barTextColor: "rgba(142, 142, 147, 1)",
+      colorPrimary: "rgba(0, 38, 255, 1)",
+      colorSecondary: "rgba(0, 38, 255, 1)",
+      fontBase: '"Open Sans", sans-serif',
+      fontCode:
+        'ui-monospace, Menlo, Monaco, "Roboto Mono", "Oxygen Mono", "Ubuntu Monospace", "Source Code Pro", "Droid Sans Mono", "Courier New", monospace',
+      inputBg: "white",
+      inputBorder: "rgba(0, 6, 29, 0.09)",
+      inputBorderRadius: 4,
+      inputTextColor: "rgba(0, 0, 0, 1)",
+      textColor: "rgba(0, 0, 0, 1)",
+      textInverseColor: "white",
+      textMutedColor: "rgba(142, 142, 147, 1)",
     },
   },
 };
