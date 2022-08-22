22454ea5a007131538140c9308390c5de6bbcfe4
Philipp Siekmann <philipp.siekmann@flipapp.de>
2022-08-22 13:39:54 +0200

    test(components): add mouseenter/leave test for FlipTooltip

--- packages/swirl-components/src/components/flip-tooltip/flip-tooltip.spec.tsx
+++ packages/swirl-components/src/components/flip-tooltip/flip-tooltip.spec.tsx
@@ -2,6 +2,29 @@ import { newSpecPage } from "@stencil/core/testing";
 
 import { FlipTooltip } from "./flip-tooltip";
 
+const expectedVisible = `
+<span class="tooltip">
+  <span aria-describedby="tooltip" class="tooltip__reference" tabindex="0">
+    <slot></slot>
+  </span>
+  <span class="tooltip__popper" style="left: 0px; top: 0px;">
+    <span class="tooltip__bubble" id="tooltip" role="tooltip">
+      <span class="tooltip__content">
+        Tooltip
+      </span>
+      <span class="tooltip__arrow"></span>
+    </span>
+  </span>
+</span>`;
+
+const expectedHidden = `
+<span class="tooltip">
+  <span aria-describedby="tooltip" class="tooltip__reference" tabindex="0">
+    <slot></slot>
+  </span>
+  <span class="tooltip__popper" style="left: 0px; top: 0px;"></span>
+</span>`;
+
 describe("flip-tooltip", () => {
   it("renders the reference", async () => {
     const page = await newSpecPage({
@@ -29,32 +52,37 @@ describe("flip-tooltip", () => {
     page.waitForChanges();
     await new Promise((resolve) => setTimeout(resolve));
 
-    expect(page.root.shadowRoot.innerHTML).toEqualHtml(`
-          <span class="tooltip">
-            <span aria-describedby="tooltip" class="tooltip__reference" tabindex="0">
-              <slot></slot>
-            </span>
-            <span class="tooltip__popper" style="left: 0px; top: 0px;">
-              <span class="tooltip__bubble" id="tooltip" role="tooltip">
-                <span class="tooltip__content">
-                  Tooltip
-                </span>
-                <span class="tooltip__arrow"></span>
-              </span>
-            </span>
-          </span>`);
+    expect(page.root.shadowRoot.innerHTML).toEqualHtml(expectedVisible);
 
     referenceEl.blur();
 
     page.waitForChanges();
     await new Promise((resolve) => setTimeout(resolve));
 
-    expect(page.root.shadowRoot.innerHTML).toEqualHtml(`
-            <span class="tooltip">
-              <span aria-describedby="tooltip" class="tooltip__reference" tabindex="0">
-                <slot></slot>
-              </span>
-              <span class="tooltip__popper" style="left: 0px; top: 0px;"></span>
-            </span>`);
+    expect(page.root.shadowRoot.innerHTML).toEqualHtml(expectedHidden);
+  });
+
+  it("shows/hides the positioned popper on mouseenter/mouseleave", async () => {
+    const page = await newSpecPage({
+      components: [FlipTooltip],
+      html: `<flip-tooltip delay="0" position="bottom" tooltip="Tooltip" tooltip-id="tooltip"><flip-badge label="Trigger"></flip-badge></flip-tooltip>`,
+    });
+
+    page.root.dispatchEvent(new MouseEvent("mouseenter"));
+
+    // wait for delay
+    await new Promise((resolve) => setTimeout(resolve));
+
+    page.waitForChanges();
+    await new Promise((resolve) => setTimeout(resolve));
+
+    expect(page.root.shadowRoot.innerHTML).toEqualHtml(expectedVisible);
+
+    page.root.dispatchEvent(new MouseEvent("mouseleave"));
+
+    page.waitForChanges();
+    await new Promise((resolve) => setTimeout(resolve));
+
+    expect(page.root.shadowRoot.innerHTML).toEqualHtml(expectedHidden);
   });
 });
