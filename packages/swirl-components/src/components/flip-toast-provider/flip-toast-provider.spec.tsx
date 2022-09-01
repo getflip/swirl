df67d5bc3a70be3d791bd15aa04d95d80c3efc22
Philipp Siekmann <philipp.siekmann@flipapp.de>
2022-09-01 13:10:48 +0200

    test(components): fix FlipToastProvider test

--- packages/swirl-components/src/components/flip-toast-provider/flip-toast-provider.spec.tsx
+++ packages/swirl-components/src/components/flip-toast-provider/flip-toast-provider.spec.tsx
@@ -29,7 +29,7 @@ describe("flip-toast-provider", () => {
     expect(page.root).toEqualHtml(`
       <flip-toast-provider role="status">
         <mock:shadow-root>
-          <flip-stack spacing="2">
+          <flip-stack spacing="12">
             <flip-toast content="Toast 1" toastid="toast-1"></flip-toast>
             <flip-toast content="Toast 2" dismisslabel="Dismiss" icon="<lip-icon-mail></flip-icon-mail>" intent="warning" toastid="toast-2"></flip-toast>
           </flip-stack>
@@ -44,7 +44,7 @@ describe("flip-toast-provider", () => {
     expect(page.root).toEqualHtml(`
       <flip-toast-provider role="status">
         <mock:shadow-root>
-          <flip-stack spacing="2">
+          <flip-stack spacing="12">
             <flip-toast content="Toast 1" toastid="toast-1"></flip-toast>
           </flip-stack>
         </mock:shadow-root>
@@ -58,7 +58,7 @@ describe("flip-toast-provider", () => {
     expect(page.root).toEqualHtml(`
       <flip-toast-provider role="status">
         <mock:shadow-root>
-          <flip-stack spacing="2"></flip-stack>
+          <flip-stack spacing="12"></flip-stack>
         </mock:shadow-root>
       </flip-toast-provider>
     `);
