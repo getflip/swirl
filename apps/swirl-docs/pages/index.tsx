9fcefe95c492aec3554d7604cb98fcb431168d42
webspaceadam <adam.gniady@flipapp.de>
2022-09-02 13:50:13 +0200

    feat(documentation): :zap: nextjs bootstrap with tailwind

---
+++ apps/swirl-docs/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <h2 className="text-5xl font-bold underline">tailwind test</h2>
+      <p className="text-5xl text-[color:var(--s-action-primary-default)]">
+        hello
+      </p>
+    </>
+  );
+};
+
+export default Home;
