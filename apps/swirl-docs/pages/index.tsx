f1479ae3b624f717fd1a95af0688df1915e67c5f
webspaceadam <adam.gniady@flipapp.de>
2022-09-05 14:49:28 +0200

    feat(documentation): :art: basic setup

--- apps/swirl-docs/pages/index.tsx
+++ apps/swirl-docs/pages/index.tsx
@@ -2,14 +2,9 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <>
-      <h2 className="text-5xl font-bold underline on-surface-1">
-        tailwind test
-      </h2>
-      <p className="text-5xl text-icon-success">hello default</p>
-      <p className="text-5xl text-icon-success-light">hello light</p>
-      <p className="text-5xl text-icon-success-dark">hello dark</p>
-    </>
+    <main className="flex justify-center items-center h-screen w-screen">
+      <h1 className="text-6xl font-bold">NextJS Bootstrap with Tailwind</h1>
+    </main>
   );
 };
 
