ae658c3585faefbea742386f0c6dca0adc2c9c40
webspaceadam <adam.gniady@flipapp.de>
2022-09-06 14:15:45 +0200

    feat: :art: more mdx

--- apps/swirl-docs/next.config.js
+++ apps/swirl-docs/next.config.js
@@ -1,5 +1,16 @@
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   reactStrictMode: true,
   swcMinify: true,
   i18n: {
@@ -8,4 +19,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withMDX(nextConfig);
