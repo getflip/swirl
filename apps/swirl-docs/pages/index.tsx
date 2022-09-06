e2aafa32dd593ef683d16a31eaef5a5dbe12a976
webspaceadam <adam.gniady@flipapp.de>
2022-09-06 11:18:02 +0200

    feat(documentation): :art: icons added again

--- apps/swirl-docs/pages/index.tsx
+++ apps/swirl-docs/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import icon from "@getflip/swirl-icons/icons/Add16.svg";
 
 const Home: NextPage = () => {
   return (
@@ -8,8 +9,9 @@ const Home: NextPage = () => {
         <title>Swirl Docs</title>
         <meta name="description" content="Swirl Docs" />
       </Head>
-      <main className="flex justify-center items-center h-screen w-screen">
+      <main className="flex flex-col justify-center items-center h-screen w-screen">
         <h1 className="text-6xl font-bold">NextJS Bootstrap with Tailwind</h1>
+        <img src={icon.src} alt="add" />
       </main>
     </>
   );
