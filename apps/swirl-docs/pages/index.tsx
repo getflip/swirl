e4572d82a07073105614941aed33f46c7f39c9d1
webspaceadam <adam.gniady@flipapp.de>
2022-09-07 15:22:39 +0200

    feat: :fire: mdx poc stuff

--- apps/swirl-docs/pages/index.tsx
+++ apps/swirl-docs/pages/index.tsx
@@ -1,8 +1,14 @@
-import type { NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import icon from "@getflip/swirl-icons/icons/Add16.svg";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+import matter from "gray-matter";
+import fs from "fs";
+import path from "path";
+import { Children } from "react";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ mdxSource, content }: any) => {
   return (
     <>
       <Head>
@@ -13,8 +19,39 @@ const Home: NextPage = () => {
         <h1 className="text-6xl font-bold">NextJS Bootstrap with Tailwind</h1>
         <img src={icon.src} alt="add" />
       </main>
+
+      <MDXRemote
+        {...mdxSource}
+        components={{
+          h1: (props: any) => (
+            <h2
+              className="text-5xl font-bold"
+              {...props}
+              id={props.children.toLowerCase()}
+            >
+              {props.children}
+            </h2>
+          ),
+        }}
+      />
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps<{
+  mdxSource: MDXRemoteSerializeResult;
+}> = async () => {
+  const POST_PATH = path.join(process.cwd(), "posts", "test.mdx");
+  const source = fs.readFileSync(POST_PATH, "utf8");
+
+  const { content } = matter(source);
+
+  const lineByLine = content.split("\n").filter((line) => line.startsWith("#"));
+
+  console.log(lineByLine);
+
+  const mdxSource = await serialize(content, { parseFrontmatter: true });
+  return { props: { mdxSource, content } };
 };
 
 export default Home;
