a5a91cdd3c1187391e8d91bfd97c3e63304c02e5
Philipp Siekmann <philipp.siekmann@flipapp.de>
2022-09-15 12:00:00 +0200

    docs(components): adjust FlipResourceListItem story

--- packages/swirl-components/src/components/flip-resource-list-item/flip-resource-list-item.stories.ts
+++ packages/swirl-components/src/components/flip-resource-list-item/flip-resource-list-item.stories.ts
@@ -18,7 +18,6 @@ const Template = (args) => {
     <flip-avatar label="John Doe" slot="media" src="https://picsum.photos/id/433/144/144"></flip-avatar>
 
     <flip-action-list slot="menu">
-      <flip-action-list-section label="Section 1">
         <flip-action-list-item
           icon="<flip-icon-mention></flip-icon-mention>"
           label="Action item 1"
@@ -27,25 +26,6 @@ const Template = (args) => {
           icon="<flip-icon-mention></flip-icon-mention>"
           label="Action item 2"
         ></flip-action-list-item>
-        <flip-action-list-item
-          icon="<flip-icon-mention></flip-icon-mention>"
-          label="Action item 3"
-        ></flip-action-list-item>
-      </flip-action-list-section>
-      <flip-action-list-section label="Section 2">
-        <flip-action-list-item
-          icon="<flip-icon-mention></flip-icon-mention>"
-          label="Action item 1"
-        ></flip-action-list-item>
-        <flip-action-list-item
-          icon="<flip-icon-mention></flip-icon-mention>"
-          label="Action item 2"
-        ></flip-action-list-item>
-        <flip-action-list-item
-          icon="<flip-icon-mention></flip-icon-mention>"
-          label="Action item 3"
-        ></flip-action-list-item>
-      </flip-action-list-section>
     </flip-action-list>
   `;
 
@@ -57,5 +37,4 @@ export const FlipResourceListItem = Template.bind({});
 FlipResourceListItem.args = {
   description: "With a description",
   label: "This is a resource item",
-  meta: "Yesterday",
 };
