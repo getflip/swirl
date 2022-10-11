a8c56173ea1e37011c6e613cd730ea3593dd1c40
Philipp Siekmann <philippsiekmann@me.com>
2022-10-11 14:20:30 +0200

    test(components). fix FlipHeading test

--- packages/swirl-components/src/components/flip-heading/flip-heading.spec.tsx
+++ packages/swirl-components/src/components/flip-heading/flip-heading.spec.tsx
@@ -12,7 +12,7 @@ describe("flip-heading", () => {
     expect(page.root).toEqualHtml(`
       <flip-heading heading-id="id" level="3">
         <mock:shadow-root>
-          <h3 class="heading heading--align-start heading--level-3" id="id"></h3>
+          <h3 class="heading heading--align-start heading--level-3" id="id" part="heading"></h3>
         </mock:shadow-root>
         Heading
       </flip-heading>
@@ -28,7 +28,7 @@ describe("flip-heading", () => {
     expect(page.root).toEqualHtml(`
       <flip-heading as="h6" heading-id="id" level="3">
         <mock:shadow-root>
-          <h6 class="heading heading--align-start heading--level-3" id="id"></h6>
+          <h6 class="heading heading--align-start heading--level-3" id="id" part="heading"></h6>
         </mock:shadow-root>
         Heading
       </flip-heading>
