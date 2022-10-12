ecb637dcdbb6008304fe979973a2ced26f4695d6
webspaceadam <adam.gniady@flipapp.de>
2022-10-12 13:18:08 +0200

    fixed build errors

--- apps/swirl-docs/src/pages/icons/components/IconGrid.tsx
+++ apps/swirl-docs/src/pages/icons/components/IconGrid.tsx
@@ -15,20 +15,20 @@ export const IconGrid: FunctionComponent<IconGridProps> = ({
   return (
     <ul className="grid grid-cols-2 md:grid-cols-fill-rows gap-4 w-full">
       {iconList?.map((icon: string, index: number) => (
-        <li key={`${icons[icon].name}-${index}`}>
+        <li key={`${icons[icon]?.name}-${index}`}>
           <a
-            href={`#${icons[icon].name}`}
-            onClick={() => handleTileClick(icons[icon].name)}
+            href={`#${icons[icon]?.name}`}
+            onClick={() => handleTileClick(icons[icon]?.name)}
             className="flex flex-col justify-center items-center py-4 border-1 rounded-lg"
           >
             <i
-              className={`swirl-icons-${icons[icon].name}28 text-icon-strong`}
+              className={`swirl-icons-${icons[icon]?.name}28 text-icon-strong`}
             ></i>
-            <span className="text-text-subdued">{icons[icon].name}</span>
+            <span className="text-text-subdued">{icons[icon]?.name}</span>
           </a>
         </li>
       ))}
-      {iconList.length === 0 && <p>No icons found</p>}
+      {iconList?.length === 0 && <p>No icons found</p>}
     </ul>
   );
 };
