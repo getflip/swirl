81dd1584e9d9548e60c0d48939e4b3bcc2ce47fe
Philipp Siekmann <philippsiekmann@me.com>
2022-10-13 09:12:26 +0200

    docs(components): add date format explanation for FlipDateInput

--- packages/swirl-components/src/components/flip-date-input/flip-date-input.stories.ts
+++ packages/swirl-components/src/components/flip-date-input/flip-date-input.stories.ts
@@ -5,6 +5,15 @@ export default {
   argTypes: {
     format: {
       description: "See https://date-fns.org/v2.29.3/docs/format.",
+      table: {
+        type: {
+          detail: '"d", "M", "y", and separators (e.g. ".", "-", "/", " ")',
+          summary: "Allowed patterns",
+        },
+      },
+    },
+    locale: {
+      control: { type: "object" },
     },
   },
   component: "flip-date-input",
