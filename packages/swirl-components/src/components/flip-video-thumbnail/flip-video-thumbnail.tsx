ddfca17617fc493b97a99ea9ef9f281d7e9597e4
Philipp Siekmann <philippsiekmann@me.com>
2022-10-13 09:22:01 +0200

    feat(components): enlarge FlipVideoThumbnail play icon for large containers

--- packages/swirl-components/src/components/flip-video-thumbnail/flip-video-thumbnail.tsx
+++ packages/swirl-components/src/components/flip-video-thumbnail/flip-video-thumbnail.tsx
@@ -37,9 +37,7 @@ export class FlipVideoThumbnail {
           <svg
             class="video-thumbnail__play-icon"
             fill="none"
-            height="40"
             viewBox="0 0 40 40"
-            width="40"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
