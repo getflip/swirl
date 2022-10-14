e336e2cb029495bb10cad7a48fbf00de8335a5ca
webspaceadam <adam.gniady@flipapp.de>
2022-10-14 08:08:13 +0200

    fixed table hydration error

--- apps/swirl-docs/src/components/Tokens/TokensList.tsx
+++ apps/swirl-docs/src/components/Tokens/TokensList.tsx
@@ -9,20 +9,24 @@ interface TokensListProps {
 export const TokensList: FunctionComponent<TokensListProps> = ({ tokens }) => {
   return (
     <table className="mb-10">
-      <tr className="hidden md:grid gap-2 grid-cols-5 items-end border-b-1 pb-4">
-        <th className="col-span-2 font-semibold">
-          <h3>Token Name</h3>
-        </th>
-        <th className="col-span-1 font-semibold">
-          <h3>Value</h3>
-        </th>
-        <th className="col-span-2 font-semibold">
-          <h3>Description</h3>
-        </th>
-      </tr>
-      {tokens.map((token: Token, index: number) => {
-        return <TokenItem key={token.name + `-${index}`} token={token} />;
-      })}
+      <thead>
+        <tr className="hidden md:grid gap-2 grid-cols-5 items-end border-b-1 pb-4">
+          <th className="col-span-2 font-semibold">
+            <h3>Token Name</h3>
+          </th>
+          <th className="col-span-1 font-semibold">
+            <h3>Value</h3>
+          </th>
+          <th className="col-span-2 font-semibold">
+            <h3>Description</h3>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {tokens.map((token: Token, index: number) => {
+          return <TokenItem key={token.name + `-${index}`} token={token} />;
+        })}
+      </tbody>
     </table>
   );
 };
