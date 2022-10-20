95106b9961b4f1b579a75cbbec0fd925354ebf34
Philipp <philipp.siekmann@flipapp.de>
2022-10-20 14:47:58 +0200

    APP-11705 web component date picker + date input (#63)

---
+++ .changeset/ten-chicken-clean.md
@@ -0,0 +1,5 @@
+---
+"@getflip/swirl-components": minor
+---
+
+Add "placement" prop to flip-popover component
---
+++ packages/swirl-components/src/components/flip-date-picker/flip-date-picker.tsx
@@ -0,0 +1,90 @@
+import {
+  Component,
+  Event,
+  EventEmitter,
+  h,
+  Host,
+  Prop,
+  Watch,
+} from "@stencil/core";
+import AirDatepicker, { AirDatepickerLocale } from "air-datepicker";
+
+import localeEn from "air-datepicker/locale/en";
+
+@Component({
+  shadow: true,
+  styleUrl: "flip-date-picker.css",
+  tag: "flip-date-picker",
+})
+export class FlipDatePicker {
+  @Prop() locale?: Partial<AirDatepickerLocale> = localeEn;
+  @Prop() maxDate?: Date;
+  @Prop() minDate?: Date;
+  @Prop() range?: boolean;
+  @Prop() startDate?: Date;
+  @Prop({ mutable: true }) value?: Date | Date[];
+
+  @Event({ bubbles: false }) valueChange: EventEmitter<Date | Date[]>;
+
+  private containerEl: HTMLDivElement;
+  private picker: AirDatepicker<HTMLElement>;
+
+  @Watch("locale")
+  @Watch("maxDate")
+  @Watch("minDate")
+  @Watch("range")
+  @Watch("startDate")
+  @Watch("value")
+  watchProps() {
+    this.init();
+  }
+
+  componentDidLoad() {
+    this.init();
+  }
+
+  disconnectedCallback() {
+    this.picker?.destroy();
+  }
+
+  private init() {
+    this.picker?.destroy();
+
+    this.picker = new AirDatepicker(this.containerEl, {
+      classes: "date-picker",
+      inline: true,
+      locale: this.locale,
+      maxDate: this.maxDate,
+      minDate: this.minDate,
+      minView: "days",
+      nextHtml: "<flip-icon-chevron-right></flip-icon-chevron-right>",
+      onSelect: ({ date }) => {
+        this.valueChange.emit(date);
+      },
+      prevHtml: "<flip-icon-chevron-left></flip-icon-chevron-left>",
+      range: this.range,
+      selectedDates:
+        this.value === undefined
+          ? undefined
+          : Array.isArray(this.value)
+          ? this.value
+          : [this.value],
+      startDate: this.startDate,
+      toggleSelected: !this.range,
+      view: "days",
+    });
+  }
+
+  private onClick = (event: MouseEvent) => {
+    event.stopPropagation();
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <Host onClick={this.onClick}>
+        <div ref={(el) => (this.containerEl = el)}></div>
+      </Host>
+    );
+  }
+}
