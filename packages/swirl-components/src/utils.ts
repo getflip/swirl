87c32cd18c08a5aefd82953859a5eb55d4ef444f
Philipp Siekmann <philippsiekmann@me.com>
2022-10-26 10:30:36 +0200

    style(components): adjust utils code style

--- packages/swirl-components/src/utils.ts
+++ packages/swirl-components/src/utils.ts
@@ -14,24 +14,29 @@ export function debounce(
   wait: number,
   immediate: boolean = false
 ) {
-  var timeout;
+  let timeout: NodeJS.Timeout;
 
   return function executedFunction() {
-    var context = this;
-    var args = arguments;
+    const context = this;
+    const args = arguments;
 
-    var later = function () {
+    const later = function () {
       timeout = null;
-      if (!immediate) func.apply(context, args);
+
+      if (!immediate) {
+        func.apply(context, args);
+      }
     };
 
-    var callNow = immediate && !timeout;
+    const callNow = immediate && !Boolean(timeout);
 
     clearTimeout(timeout);
 
     timeout = setTimeout(later, wait);
 
-    if (callNow) func.apply(context, args);
+    if (callNow) {
+      func.apply(context, args);
+    }
   };
 }
 
