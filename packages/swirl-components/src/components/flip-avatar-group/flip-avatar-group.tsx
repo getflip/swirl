69607f061d62174d81a0d3c8c599709700ab14ac
Philipp <philipp.siekmann@flipapp.de>
2022-10-28 16:43:08 +0200

    Web Components: minor style adjustments (#74)

--- packages/swirl-components/src/components/flip-avatar-group/flip-avatar-group.tsx
+++ packages/swirl-components/src/components/flip-avatar-group/flip-avatar-group.tsx
@@ -12,6 +12,22 @@ import classnames from "classnames";
 export class FlipAvatarGroup {
   @Prop() badge?: string;
 
+  private badgeEl: HTMLElement;
+
+  componentDidLoad() {
+    this.forceBadgeProps();
+  }
+
+  private forceBadgeProps() {
+    if (!Boolean(this.badge)) {
+      return;
+    }
+
+    const badge = this.badgeEl.querySelector("flip-badge");
+
+    badge?.setAttribute("size", "m");
+  }
+
   render() {
     const className = classnames("avatar-group", {
       "avatar-group--has-badge": Boolean(this.badge),
@@ -24,7 +40,11 @@ export class FlipAvatarGroup {
         <div class={className} role="group">
           <slot></slot>
           {this.badge && (
-            <span class={badgeClassName} innerHTML={this.badge}></span>
+            <span
+              class={badgeClassName}
+              innerHTML={this.badge}
+              ref={(el) => (this.badgeEl = el)}
+            ></span>
           )}
         </div>
       </Host>
