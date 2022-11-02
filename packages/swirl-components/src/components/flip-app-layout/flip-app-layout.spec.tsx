51e1d8bab46900be09bc26118e52b09b003ceb83
Philipp Siekmann <philippsiekmann@me.com>
2022-11-02 16:17:40 +0100

    test(components): fix unit test

--- packages/swirl-components/src/components/flip-app-layout/flip-app-layout.spec.tsx
+++ packages/swirl-components/src/components/flip-app-layout/flip-app-layout.spec.tsx
@@ -18,16 +18,15 @@ describe("flip-app-layout", () => {
     expect(page.root).toEqualHtml(`
       <flip-app-layout>
         <mock:shadow-root>
-          <section aria-labelledby="app-name" class="app-layout app-layout--mobile-view-body" role="document" tabindex="0">
+          <section class="app-layout app-layout--mobile-view-body" role="document" tabindex="0">
             <div class="app-layout__grid">
               <header class="app-layout__header">
                 <flip-heading as="h1" headingid="app-name" level="2"></flip-heading>
-                <flip-button class="app-layout__cta" intent="primary" variant="flat"></flip-button>
               </header>
               <nav class="app-layout__navigation">
                 <slot name="navigation"></slot>
               </nav>
-              <section aria-labelledby="heading" class="app-layout__body">
+              <section class="app-layout__body">
                 <header class="app-layout__app-bar">
                   <div class="app-layout__app-bar-heading">
                     <flip-heading as="h2" headingid="heading" level="4"></flip-heading>
@@ -40,9 +39,9 @@ describe("flip-app-layout", () => {
                   <slot name="content"></slot>
                 </div>
               </section>
-              <aside aria-labelledby="sidebar-heading" class="app-layout__sidebar">
+              <aside class="app-layout__sidebar">
                 <header class="app-layout__sidebar-header">
-                  <flip-button class="app-layout__sidebar-close-button" icon="<flip-icon-close></flip-icon-close>" intent="primary"></flip-button>
+                  <flip-button class="app-layout__sidebar-close-button" icon="<flip-icon-close></flip-icon-close>" intent="primary" label="Close sidebar"></flip-button>
                   <flip-heading as="h3" headingid="sidebar-heading" level="2"></flip-heading>
                 </header>
                 <div class="app-layout__sidebar-content">
