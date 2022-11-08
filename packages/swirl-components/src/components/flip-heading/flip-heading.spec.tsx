9803103d15a08a1bcd15c8f029ecbb35484dcd3c
Philipp Siekmann <philippsiekmann@me.com>
2022-11-08 10:07:05 +0100

    test(components): fix unit test

--- packages/swirl-components/src/components/flip-heading/flip-heading.spec.tsx
+++ packages/swirl-components/src/components/flip-heading/flip-heading.spec.tsx
@@ -12,7 +12,7 @@ describe("flip-heading", () => {
     expect(page.root).toEqualHtml(`
       <flip-heading heading-id="id" level="3">
         Heading
-        <h3 class="heading heading--align-start heading--level-3" id="id" part="heading"></h3>
+        <h3 class="heading heading--align-start heading--level-3" id="id"></h3>
       </flip-heading>
     `);
   });
@@ -26,7 +26,7 @@ describe("flip-heading", () => {
     expect(page.root).toEqualHtml(`
       <flip-heading as="h6" heading-id="id" level="3">
         Heading
-        <h6 class="heading heading--align-start heading--level-3" id="id" part="heading"></h6>
+        <h6 class="heading heading--align-start heading--level-3" id="id"></h6>
       </flip-heading>
     `);
   });
