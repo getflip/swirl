9d16bce0d34963e0e87ca018708950888160db97
webspaceadam <adam.gniady@flipapp.de>
2022-12-07 10:43:26 +0100

    feat(documentation): :art: removed server side generation of ToC and added a toc hook to handle the ToC State

--- apps/swirl-docs/src/components/Layout/DocLinksNav.tsx
+++ apps/swirl-docs/src/components/Layout/DocLinksNav.tsx
@@ -47,7 +47,7 @@ export const DocLinksNav: FunctionComponent<DocLinksNavProps> = ({
                     }
                   `}
                   >
-                    {link.name}
+                    {link.title}
                   </a>
                 </Link>
               </li>
