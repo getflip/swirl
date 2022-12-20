08852cf93787d9105abd3c19da542acbfc91c9a8
webspaceadam <adam.gniady@flipapp.de>
2022-12-20 10:52:16 +0100

    chore(documentation): :art: added interface for window errors

--- apps/swirl-docs/src/lib/gtm/index.tsx
+++ apps/swirl-docs/src/lib/gtm/index.tsx
@@ -1,7 +1,10 @@
 export const GTM_ID = "GTM-TNKB82Q";
 
-// here to get rid of misleading typescript errors
-declare const window: any;
+declare global {
+  interface Window {
+    dataLayer: Record<string, any>;
+  }
+}
 
 export const pageview = (url: any) => {
   window.dataLayer.push({
