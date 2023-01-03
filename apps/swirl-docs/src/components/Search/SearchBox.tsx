58cc98496a60244c22330ce5fc5f30e7f42253d3
webspaceadam <adam.gniady@flipapp.de>
2023-01-03 11:52:05 +0100

    added hidden label for search box

--- apps/swirl-docs/src/components/Search/SearchBox.tsx
+++ apps/swirl-docs/src/components/Search/SearchBox.tsx
@@ -4,7 +4,11 @@ import { connectSearchBox } from "react-instantsearch-dom";
 function SearchBox({ refine }: any) {
   return (
     <>
+      <label className="hidden" id="algolia_label" htmlFor="algolia_search">
+        Search
+      </label>
       <input
+        aria-labelledby="algolia_label"
         id="algolia_search"
         type="search"
         placeholder="Search for articles!"
