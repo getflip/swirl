cd169e47b6328b7848317682e9be0eb2d6a2f16f
webspaceadam <adam.gniady@flipapp.de>
2023-01-04 09:25:19 +0100

    added as const instead of enum

--- apps/swirl-docs/src/lib/search/index.ts
+++ apps/swirl-docs/src/lib/search/index.ts
@@ -1,3 +1,5 @@
-export enum ALGOLIA_INDEX {
-  DEV = "dev_swirl-docs",
-}
+export const ALGOLIA_INDEX = {
+  DEV: "dev_swirl-docs",
+  PROD: "prod_swirl-docs",
+} as const;
+export type AlgoliaIndex = keyof typeof ALGOLIA_INDEX;
