05707a17252b9a26f85f590fdadbfdfa4af39ac7
Philipp Siekmann <philippsiekmann@me.com>
2023-01-10 13:51:28 +0100

    test(components): fix tests

--- packages/swirl-components/src/components/flip-option-list-item/flip-option-list-item.spec.tsx
+++ packages/swirl-components/src/components/flip-option-list-item/flip-option-list-item.spec.tsx
@@ -19,11 +19,11 @@ describe("flip-option-list-item", () => {
     expect(page.root).toEqualHtml(`
       <flip-option-list-item context="single-select" disabled="false" icon="<flip-icon-mention></flip-icon-mention>" label="Option List Item" selected="true" value="Value">
         <mock:shadow-root>
-          <div aria-selected="true" class="option-list-item option-list-item--context-single-select option-list-item--selected" role="option">
+          <div aria-selected="true" class="option-list-item option-list-item--context-single-select option-list-item--selected" part="option-list-item" role="option">
             <span class="option-list-item__icon">
               <flip-icon-mention size="24"></flip-icon-mention>
             </span>
-            <span class="option-list-item__label">
+            <span class="option-list-item__label" part="option-list-item__label">
               Option List Item
             </span>
             <span class="option-list-item__selection-icon">
--- packages/swirl-components/src/components/flip-resource-list-item/flip-resource-list-item.spec.tsx
+++ packages/swirl-components/src/components/flip-resource-list-item/flip-resource-list-item.spec.tsx
@@ -19,7 +19,7 @@ describe("flip-resource-list-item", () => {
       <flip-resource-list-item description="Description" label="Label" media="<flip-avatar label=&quot;John Doe&quot; src=&quot;https://picsum.photos/id/433/144/144&quot;></flip-avatar>" role="row">
         <mock:shadow-root>
           <div class="resource-list-item" role="gridcell">
-            <button aria-labelledby="label" class="resource-list-item__content" tabindex="0">
+            <button aria-labelledby="label" class="resource-list-item__content" part="resource-list-item__content"  tabindex="0">
               <span class="resource-list-item__media">
                 <flip-avatar label="John Doe" src="https://picsum.photos/id/433/144/144"></flip-avatar>
               </span>
