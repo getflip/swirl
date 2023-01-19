deab79d539a06b328d4d1dadf7b98492e608f576
Adam Silber-Gniady <adam.gniady@flipapp.de>
2023-01-19 14:05:05 +0100

    feat(documentation): :art: layout design improvments (#148)

--- apps/swirl-docs/src/components/Documentation/DocumentationHeader.tsx
+++ apps/swirl-docs/src/components/Documentation/DocumentationHeader.tsx
@@ -10,11 +10,13 @@ export const DocumentationHeader: FunctionComponent<
 > = ({ frontMatter }) => {
   return (
     <header className="border-b-1 pb-12 mb-12">
-      <div className="mb-1 inline-flex items-center">
-        <h1 className="text-4xl text-text-default">{frontMatter.title}</h1>
+      <div className="mb-space-8 inline-flex items-center">
+        <h1 className="text-4xl text-text-default font-font-weight-bold">
+          {frontMatter.title}
+        </h1>
         {frontMatter.tags?.map((tag) => (
           <span
-            className="bg-surface-neutral-subdued px-2 py-1 rounded-md ml-2 font-medium text-sm"
+            className="bg-surface-neutral-subdued px-2 py-1 rounded-md ml-3 font-medium text-sm"
             key={tag}
           >
             {tag}
