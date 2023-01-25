1b4151d87a6b49c6be52e68cc9125588cab5b012
Philipp Siekmann <philippsiekmann@me.com>
2023-01-25 15:25:58 +0100

    test(components): fix tests

--- packages/swirl-components/src/components/swirl-skeleton-text/swirl-skeleton-text.spec.tsx
+++ packages/swirl-components/src/components/swirl-skeleton-text/swirl-skeleton-text.spec.tsx
@@ -12,11 +12,11 @@ describe("swirl-skeleton-text", () => {
     expect(page.root).toEqualHtml(`
       <swirl-skeleton-text lines="4" size="lg">
         <mock:shadow-root>
-          <div class="skeleton-text skeleton-text--size-lg">
-            <div class="skeleton-text__line"></div>
-            <div class="skeleton-text__line"></div>
-            <div class="skeleton-text__line"></div>
-            <div class="skeleton-text__line"></div>
+          <div class="skeleton-text skeleton-text--size-lg" part="skeleton-text skeleton-text--size-lg">
+            <div class="skeleton-text__line skeleton-text__line--size-lg" part="skeleton-text__line skeleton-text__line--size-lg"></div>
+            <div class="skeleton-text__line skeleton-text__line--size-lg" part="skeleton-text__line skeleton-text__line--size-lg"></div>
+            <div class="skeleton-text__line skeleton-text__line--size-lg" part="skeleton-text__line skeleton-text__line--size-lg"></div>
+            <div class="skeleton-text__line skeleton-text__line--size-lg" part="skeleton-text__line skeleton-text__line--size-lg"></div>
           </div>
         </mock:shadow-root>
       </swirl-skeleton-text>
@@ -32,8 +32,8 @@ describe("swirl-skeleton-text", () => {
     expect(page.root).toEqualHtml(`
       <swirl-skeleton-text animated="false">
         <mock:shadow-root>
-          <div class="skeleton-text skeleton-text--size-base skeleton-text--static">
-            <div class="skeleton-text__line"></div>
+          <div class="skeleton-text skeleton-text--size-base skeleton-text--static" part="skeleton-text skeleton-text--size-base skeleton-text--static">
+            <div class="skeleton-text__line skeleton-text__line--size-base" part="skeleton-text__line skeleton-text__line--size-base"></div>
           </div>
         </mock:shadow-root>
       </swirl-skeleton-text>
