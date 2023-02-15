fc1e7086852f3c2e0378d626781276212b0f19a6
Philipp Siekmann <philippsiekmann@me.com>
2023-02-15 12:32:03 +0100

    test(components): fix unit test

--- packages/swirl-components/src/components/swirl-shell-layout/swirl-shell-layout.spec.tsx
+++ packages/swirl-components/src/components/swirl-shell-layout/swirl-shell-layout.spec.tsx
@@ -18,29 +18,31 @@ describe("swirl-shell-layout", () => {
     expect(page.root).toEqualHtml(`
       <swirl-shell-layout>
         <mock:shadow-root>
-          <div class="shell-layout shell-layout--collapsed-sidebar">
-            <div class="shell-layout__sidebar">
-              <header class="shell-layout__header">
-                <div class="shell-layout__logo-bar">
-                  <div class="shell-layout__expanded-logo">
-                    <slot name="logo-expanded"></slot>
+          <div class="shell-layout shell-layout--collapsed-sidebar shell-layout--collapsing">
+            <div class="shell-layout__sidebar-wrapper">
+              <div class="shell-layout__sidebar">
+                <header class="shell-layout__header">
+                  <div class="shell-layout__logo-bar">
+                    <div class="shell-layout__expanded-logo">
+                      <slot name="logo-expanded"></slot>
+                    </div>
+                    <div class="shell-layout__collapsed-logo">
+                      <slot name="logo-collapsed"></slot>
+                    </div>
+                    <div class="shell-layout__toggle">
+                      <swirl-button hidelabel="" icon="<swirl-icon-double-arrow-right></swirl-icon-double-arrow-right>" label="Toggle sidebar" swirlariaexpanded="false"></swirl-button>
+                    </div>
                   </div>
-                  <div class="shell-layout__collapsed-logo">
-                    <slot name="logo-collapsed"></slot>
+                  <div class="shell-layout__tools">
+                    <slot name="tools"></slot>
                   </div>
-                  <div class="shell-layout__toggle">
-                    <swirl-button hidelabel="" icon="<swirl-icon-double-arrow-right></swirl-icon-double-arrow-right>" label="Toggle sidebar" swirlariaexpanded="false"></swirl-button>
-                  </div>
+                </header>
+                <nav aria-label="Main" class="shell-layout__main-navigation">
+                  <slot name="main-navigation"></slot>
+                </nav>
+                <div class="shell-layout__mobile-toggle">
+                  <swirl-button hidelabel="" icon="<swirl-icon-menu></swirl-icon-menu>" label="Toggle sidebar" swirlariaexpanded="false"></swirl-button>
                 </div>
-                <div class="shell-layout__tools">
-                  <slot name="tools"></slot>
-                </div>
-              </header>
-              <nav aria-label="Main" class="shell-layout__main-navigation">
-                <slot name="main-navigation"></slot>
-              </nav>
-              <div class="shell-layout__mobile-toggle">
-                <swirl-button hidelabel="" icon="<swirl-icon-menu></swirl-icon-menu>" label="Toggle sidebar" swirlariaexpanded="false"></swirl-button>
               </div>
             </div>
             <main class="shell-layout__main">
