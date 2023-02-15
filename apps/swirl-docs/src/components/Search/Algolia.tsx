675c11c3cad98d73c504bbb39080a1a04cedf236
webspaceadam <adam.gniady@flipapp.de>
2023-02-15 09:09:11 +0100

    (fix): wrong envs removed

--- apps/swirl-docs/src/components/Search/Algolia.tsx
+++ apps/swirl-docs/src/components/Search/Algolia.tsx
@@ -3,32 +3,11 @@ import algoliasearch from "algoliasearch/lite";
 import { InstantSearch } from "react-instantsearch-dom";
 import { ALGOLIA_INDEX } from "@swirl/lib/search";
 import { Autocomplete } from "./AutoComplete";
-import { isProd } from "@swirl/lib/gtm";
 
 function createAlgoliaClient() {
-  console.log(
-    "env variables: next",
-    process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
-    process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY
-  );
-  console.log(
-    "env variables: appsetting",
-    process.env.APPSETTING_NEXT_PUBLIC_ALGOLIA_APP_ID,
-    process.env.APPSETTING_NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY
-  );
-
-  if (!isProd) {
-    const searchClient = algoliasearch(
-      process.env.NEXT_PUBLIC_ALGOLIA_APP_ID!!,
-      process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY!!
-    );
-
-    return searchClient;
-  }
-
   const searchClient = algoliasearch(
-    process.env.APPSETTING_NEXT_PUBLIC_ALGOLIA_APP_ID!!,
-    process.env.APPSETTING_NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY!!
+    process.env.NEXT_PUBLIC_ALGOLIA_APP_ID!!,
+    process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY!!
   );
 
   return searchClient;
