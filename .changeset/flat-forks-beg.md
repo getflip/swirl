31203e059b8147a1a900d6dd4c004fb349b5135f
Philipp <philipp.siekmann@flipapp.de>
2023-05-12 12:21:25 +0200

    Tweak SwirlFormControl styles for label position "outside" (#255)

---
+++ .changeset/flat-forks-beg.md
@@ -0,0 +1,7 @@
+---
+"@getflip/swirl-components": minor
+"@getflip/swirl-components-angular": minor
+"@getflip/swirl-components-react": minor
+---
+
+Tweak swirl-form-control styles for label position "outside"
--- packages/swirl-components/src/components.d.ts
+++ packages/swirl-components/src/components.d.ts
@@ -1210,6 +1210,7 @@ export namespace Components {
         "min"?: number;
         "mode"?: SwirlTextInputMode1;
         "passwordToggleLabel"?: string;
+        "placeholder"?: string;
         "prefixLabel"?: string;
         "required"?: boolean;
         "rows"?: number;
@@ -3888,6 +3889,7 @@ declare namespace LocalJSX {
         "onInputFocus"?: (event: SwirlTextInputCustomEvent<FocusEvent>) => void;
         "onValueChange"?: (event: SwirlTextInputCustomEvent<string>) => void;
         "passwordToggleLabel"?: string;
+        "placeholder"?: string;
         "prefixLabel"?: string;
         "required"?: boolean;
         "rows"?: number;
