41cb37dc0948c85c11eff2948de73fb6dc2ad79a
Philipp Siekmann <philipp.siekmann@getflip.com>
2023-08-02 15:10:55 +0200

    feat(components): add remove button, timestamp and size "xl" to SwirlThumbnail

--- packages/swirl-components/src/components/swirl-thumbnail/swirl-thumbnail.tsx
+++ packages/swirl-components/src/components/swirl-thumbnail/swirl-thumbnail.tsx
@@ -3,7 +3,7 @@ import classnames from "classnames";
 
 export type SwirlThumbnailFormat = "portrait" | "landscape" | "square";
 
-export type SwirlThumbnailSize = "s" | "m" | "l";
+export type SwirlThumbnailSize = "s" | "m" | "l" | "xl";
 
 @Component({
   shadow: true,
@@ -13,25 +13,61 @@ export type SwirlThumbnailSize = "s" | "m" | "l";
 export class SwirlThumbnail {
   @Prop() alt!: string;
   @Prop() format?: SwirlThumbnailFormat = "landscape";
-  @Prop() size: SwirlThumbnailSize = "m";
+  @Prop() progress?: number;
+  @Prop() progressLabel?: string = "Loading progress";
+  @Prop() removeButtonLabel?: string = "Remove";
+  @Prop() showRemoveButton?: boolean;
+  @Prop() size?: SwirlThumbnailSize = "m";
   @Prop() src!: string;
+  @Prop() timestamp?: string;
 
   render() {
+    const showRemoveButton =
+      this.showRemoveButton && this.size === "xl" && this.format === "square";
+
+    const showTimestamp =
+      Boolean(this.timestamp) && this.size === "xl" && this.format === "square";
+
     const className = classnames(
       "thumbnail",
       `thumbnail--format-${this.format}`,
-      `thumbnail--size-${this.size}`
+      `thumbnail--size-${this.size}`,
+      {
+        "thumbnail--has-progress": this.progress !== undefined,
+      }
     );
 
     return (
       <Host>
-        <span class={className}>
+        <span class={className} role="group">
           <img
             alt={this.alt}
             class="thumbnail__image"
             loading="lazy"
             src={this.src}
           />
+          {this.progress !== undefined && (
+            <span class="thumbnail__progress-indicator">
+              <swirl-progress-indicator
+                label={this.progressLabel}
+                value={this.progress}
+              ></swirl-progress-indicator>
+            </span>
+          )}
+          {showRemoveButton && (
+            <span class="thumbnail__remove-button">
+              <swirl-button
+                hideLabel
+                icon="<swirl-icon-close></swirl-icon-close>"
+                label={this.removeButtonLabel}
+                pill
+                variant="on-image"
+              ></swirl-button>
+            </span>
+          )}
+          {showTimestamp && (
+            <span class="thumbnail__timestamp">{this.timestamp}</span>
+          )}
         </span>
       </Host>
     );
