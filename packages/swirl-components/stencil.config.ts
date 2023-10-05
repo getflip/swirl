e9d74565dfdd62bd4a2989916a137d7733d38def
Philipp Siekmann <philipp.siekmann@getflip.com>
2023-10-05 13:58:29 +0200

    feat(components): add SwirlColorInput to angularValueAccessorBindings

--- packages/swirl-components/stencil.config.ts
+++ packages/swirl-components/stencil.config.ts
@@ -35,6 +35,7 @@ const angularValueAccessorBindings: ValueAccessorConfig[] = [
   {
     elementSelectors: [
       "swirl-autocomplete:not([multi-select]):not([multiSelect])",
+      "swirl-color-input",
       "swirl-date-input",
       "swirl-radio-group",
       "swirl-search",
