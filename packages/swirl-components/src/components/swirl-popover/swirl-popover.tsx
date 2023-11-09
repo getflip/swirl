3fd861f6c1869839281e91543d9528815c584c20
Adam Silber-Gniady <adam.silber-gniady@getflip.com>
2023-11-09 14:30:04 +0100

    App 18473 investigate date input bug on i os for absence integration (#447)

--- packages/swirl-components/src/components/swirl-popover/swirl-popover.tsx
+++ packages/swirl-components/src/components/swirl-popover/swirl-popover.tsx
@@ -87,21 +87,29 @@ export class SwirlPopover {
     }
 
     const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    const isWKWebView = "webkit" in window;
+    const isSafariOrWKWebView = isSafari || isWKWebView;
+
     const target = event.target as HTMLElement;
     const relatedTarget = event.relatedTarget as HTMLElement;
     const activeElement = getActiveElement();
 
-    const popoverLostFocus =
-      !this.el.contains(target) &&
-      !this.el.contains(activeElement) &&
-      target !== this.triggerEl &&
-      !this.triggerEl?.contains(target) &&
-      (!isSafari ||
-        (isSafari &&
-          !this.el.contains(relatedTarget || target) &&
-          relatedTarget !== this.el));
+    // Check if the focus has moved outside the popover or its trigger.
+    const focusIsOutsidePopover =
+      !this.el.contains(target) && !this.el.contains(activeElement);
+    const focusIsNotOnTrigger =
+      target !== this.triggerEl && !this.triggerEl?.contains(target);
+    const extraCheckForSafariOrWKWebView =
+      isSafariOrWKWebView &&
+      !this.el.contains(relatedTarget || target) &&
+      relatedTarget !== this.el;
 
-    if (popoverLostFocus) {
+    // Close the popover if the focus is outside and additional checks for Safari or WKWebView pass.
+    if (
+      focusIsOutsidePopover &&
+      focusIsNotOnTrigger &&
+      (!isSafariOrWKWebView || extraCheckForSafariOrWKWebView)
+    ) {
       this.close();
     }
   }
@@ -165,6 +173,7 @@ export class SwirlPopover {
    */
   @Method()
   public async open(triggerEl?: HTMLElement) {
+    console.log("open", triggerEl);
     this.triggerEl = triggerEl || this.triggerEl;
 
     if (this.active || !Boolean(this.triggerEl)) {
