0b8cc1e4bbd1da32259b9cfdf328fa782780932c
webspaceadam <adam.silber-gniady@getflip.com>
2023-11-16 11:34:47 +0100

    fix(scripts): removed debugging echos

--- apps/swirl-docs/deploy-script.sh
+++ apps/swirl-docs/deploy-script.sh
@@ -1,19 +1,6 @@
-# Print the current environment variables for debugging
-echo "Current Environment Variables:"
-printenv
-
 # Check if DEPLOY_HOOK_TRIGGERED environment variable is set
-echo "Checking if DEPLOY_HOOK_TRIGGERED is set..."
 if [ -z "$DEPLOY_HOOK_TRIGGERED" ]; then
-    echo "DEPLOY_HOOK_TRIGGERED is not set. Checking for changes in ./src..."
-    # List current directory and check for changes
-    ls
     git diff HEAD^ HEAD --quiet ./src
-    # Capture the exit status of git diff
-    GIT_DIFF_EXIT_STATUS=$?
-    echo "Git diff exit status: $GIT_DIFF_EXIT_STATUS"
-    # Exit with the git diff exit status
-    exit $GIT_DIFF_EXIT_STATUS
 else
     # Bypass ignore build step
     echo "Deploy hook triggered, proceeding with build"
