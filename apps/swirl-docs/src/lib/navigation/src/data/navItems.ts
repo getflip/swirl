0ae05501bec8be19f812d81e3ffacd528069158c
Hanno Fellmann <mail@hanno-fellmann.de>
2023-12-18 15:46:29 +0100

    push

--- apps/swirl-docs/src/lib/navigation/src/data/navItems.ts
+++ apps/swirl-docs/src/lib/navigation/src/data/navItems.ts
@@ -1,7 +1,7 @@
 import { CategoryEnum, NavItem } from "../navigation.model";
+import { apiDocsNavItems } from "./apiDocs.data";
 import { apiSpecsNavItems } from "./apiSpecs.data";
 import { tokensNavItems } from "./tokens.data";
-import { apiDocsNavItems } from "./apiDocs.data";
 
 export const navItems: NavItem[] = [
   {
@@ -24,6 +24,6 @@ export const navItems: NavItem[] = [
     isRoot: true,
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. The quick brown fox jumps over the lazy dog.",
-    children: apiSpecsNavItems,
+    children: [...apiDocsNavItems, ...apiSpecsNavItems],
   },
 ];
