39f81443bf478fe4ddcd4abbbb8a81ef46d8ea58
Hanno Fellmann <mail@hanno-fellmann.de>
2024-01-04 15:19:59 +0100

    add generated files again

---
+++ apps/swirl-docs/src/lib/navigation/src/data/apiDocs.data.ts
@@ -0,0 +1,14 @@
+import { NavItem } from "../navigation.model";
+
+export const apiDocsNavItems: NavItem[] = [
+  {
+    title: "API Overview",
+    mdxFilename: "00-api-overview",
+    url: "/api-docs/docs/api-overview",
+  },
+  {
+    title: "Flip Monorepo to getflip.dev Testfile",
+    mdxFilename: "01-getting-started-test",
+    url: "/api-docs/docs/getting-started-test",
+  },
+];
