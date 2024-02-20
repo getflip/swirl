4d6c99706cc6043147244b97704e26448671d913
Philipp Siekmann <philipp.siekmann@getflip.com>
2024-02-20 15:15:54 +0100

    chore: add missing stuff to publis swirl angular api

--- packages/swirl-components-angular/projects/component-library/src/public-api.ts
+++ packages/swirl-components-angular/projects/component-library/src/public-api.ts
@@ -2,8 +2,10 @@
  * Public API Surface of component-library
  */
 
-export * from "./lib/stencil-generated/components";
+export * from "./lib/component-library.module";
+export * from "./lib/stencil-generated";
 export * from "./lib/stencil-generated/boolean-value-accessor";
+export * from "./lib/stencil-generated/components";
 export * from "./lib/stencil-generated/select-value-accessor";
 export * from "./lib/stencil-generated/text-value-accessor";
-export * from "./lib/component-library.module";
+export * from "./lib/stencil-generated/value-accessor";
