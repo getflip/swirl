1f6c1d133c82cc2891301a2acc8b9427951fcc6b
matvlaovi-flip <matej.vlaovic@getflip.com>
2024-11-05 08:40:08 +0100

    ENG-422 Create `swirl-toggle-button` and `swirl-toggle-group` components (#838)

---
+++ packages/swirl-components/src/components/swirl-toggle-button/swirl-toggle-button.stories.ts
@@ -0,0 +1,26 @@
+import { generateStoryElement } from "../../utils";
+import Docs from "./swirl-toggle-button.mdx";
+
+export default {
+  component: "swirl-toggle-button",
+  tags: ["autodocs"],
+  parameters: {
+    docs: {
+      page: Docs,
+    },
+  },
+  title: "Components/SwirlToggleButton",
+};
+
+const Template = (args) => {
+  const element = generateStoryElement("swirl-toggle-button", args);
+
+  return element;
+};
+
+export const SwirlToggleButton = Template.bind({});
+
+SwirlToggleButton.args = {
+  label: "Label",
+  identifier: "identifier",
+};
---
+++ packages/swirl-components/src/components/swirl-toggle-button/swirl-toggle-button.tsx
@@ -0,0 +1,34 @@
+import { Component, h, Prop } from "@stencil/core";
+import classnames from "classnames";
+
+@Component({
+  shadow: true,
+  styleUrl: "swirl-toggle-button.css",
+  tag: "swirl-toggle-button",
+})
+export class SwirlToggleButton {
+  @Prop() identifier!: string;
+  @Prop() label!: string;
+  @Prop({ mutable: true }) isPressed = false;
+
+  private onClick = (): void => {
+    this.isPressed = true;
+  };
+
+  render() {
+    const classNames = classnames("button", {
+      "button--pressed": this.isPressed,
+    });
+
+    return (
+      <button
+        class={classNames}
+        type="button"
+        aria-pressed={this.isPressed ? "true" : "false"}
+        onClick={this.onClick}
+      >
+        <span class="button__label">{this.label}</span>
+      </button>
+    );
+  }
+}
