edf5fe49285a459fbc5eff06540aaa55fef07049
Tobias Bahls <tobias.bahls@gmail.com>
2024-11-12 15:03:27 +0100

    getflip.dev - Fix display of nested arrays (#849)
    Co-authored-by: Tobias Bahls <tobias.bahls@getflip.com>

--- apps/swirl-docs/src/components/Documentation/EndpointDescription/Parameter.tsx
+++ apps/swirl-docs/src/components/Documentation/EndpointDescription/Parameter.tsx
@@ -1,7 +1,5 @@
-import { SwirlIconAdd, SwirlIconRemove } from "@getflip/swirl-components-react";
 import classNames from "classnames";
-import { AnimatePresence, motion } from "framer-motion";
-import { ReactNode, useState } from "react";
+import { ReactNode } from "react";
 import { Tag } from "../../Tags";
 import { DocumentationMarkdown } from "../DocumentationMarkdown";
 import {
@@ -10,6 +8,7 @@ import {
   isValidStatusCode,
 } from "../HttpStatusCodeMapper";
 import { isProdDeployment } from "@swirl/lib/env";
+import { Expandable } from "../Expandable";
 
 interface ParameterProps {
   children?: ReactNode;
@@ -19,6 +18,7 @@ interface ParameterProps {
   hidden?: boolean;
   description?: string;
   enumValues?: string[];
+  array?: boolean;
 }
 
 export function Parameter({
@@ -29,13 +29,8 @@ export function Parameter({
   required,
   hidden,
   enumValues,
+  array,
 }: ParameterProps) {
-  const [isExpanded, setIsExpanded] = useState(false);
-
-  function toggle() {
-    setIsExpanded((expanded) => !expanded);
-  }
-
   if (isProdDeployment && hidden) {
     return null;
   }
@@ -63,7 +58,7 @@ export function Parameter({
           {isValidStatusCode(name) &&
             `${getStatusText(Number(name) as HttpStatusCode)}`}
         </code>
-        {type && <Tag content={type} />}
+        {type && <Tag content={type + (array ? "[]" : "")} />}
         {required && <Tag content="required" scheme="critical" />}
         {hidden && <Tag content="hidden for public" scheme="info" /> }
       </div>
@@ -73,66 +68,37 @@ export function Parameter({
         </DocumentationMarkdown>
       )}
       {enumValues?.length && (
-        <div className="mt-space-8">
-          <div
-            className={classNames(
-              "border-border-1 border-border-default p-4",
-              "first-of-type:rounded-t-border-radius-sm last-of-type:rounded-b-border-radius-sm",
-              "border-b-0 last-of-type:border-border-1",
-              "only:rounded-border-radius-sm only:border-border-1"
-            )}
-          >
+        <Expandable>
+          <div className="mt-space-8">
+            <div
+              className={classNames(
+                "border-border-1 border-border-default p-4",
+                "first-of-type:rounded-t-border-radius-sm last-of-type:rounded-b-border-radius-sm",
+                "border-b-0 last-of-type:border-border-1",
+                "only:rounded-border-radius-sm only:border-border-1"
+              )}
+            >
             <span className="font-font-weight-medium text-font-size-sm text-text-subdued">
               Possible enum values
             </span>
+            </div>
+            <div
+              className={classNames(
+                "flex flex-wrap gap-y-space-8",
+                "border-border-1 border-border-default p-4",
+                "first-of-type:rounded-t-border-radius-sm last-of-type:rounded-b-border-radius-sm",
+                "border-b-0 last-of-type:border-border-1",
+                "only:rounded-border-radius-sm only:border-border-1"
+              )}
+            >
+              {enumValues.map((value) => (
+                <Tag key={value} content={value} />
+              ))}
+            </div>
           </div>
-          <div
-            className={classNames(
-              "flex flex-wrap gap-y-space-8",
-              "border-border-1 border-border-default p-4",
-              "first-of-type:rounded-t-border-radius-sm last-of-type:rounded-b-border-radius-sm",
-              "border-b-0 last-of-type:border-border-1",
-              "only:rounded-border-radius-sm only:border-border-1"
-            )}
-          >
-            {enumValues.map((value) => (
-              <Tag key={value} content={value} />
-            ))}
-          </div>
-        </div>
+        </Expandable>
       )}
-      {children && (
-        <div className="mt-space-8 empty:mt-0">
-          <button
-            className="inline-flex items-center gap-space-4 font-medium text-font-size-sm text-interactive-primary-default"
-            onClick={toggle}
-            type="button"
-          >
-            {!isExpanded && (
-              <>
-                <SwirlIconAdd size={20} /> Expand
-              </>
-            )}
-            {isExpanded && (
-              <>
-                <SwirlIconRemove size={20} /> Collapse
-              </>
-            )}
-          </button>
-          <AnimatePresence>
-            {isExpanded && (
-              <motion.div
-                initial={{ height: 0, opacity: 0, marginTop: 0 }}
-                animate={{ height: "auto", opacity: 1, marginTop: 16 }}
-                exit={{ height: 0, opacity: 0, marginTop: 0 }}
-                style={{ originY: 0, overflow: "hidden" }}
-              >
-                {children}
-              </motion.div>
-            )}
-          </AnimatePresence>
-        </div>
-      )}
+      {children && <Expandable>{children}</Expandable>}
     </div>
   );
 }
