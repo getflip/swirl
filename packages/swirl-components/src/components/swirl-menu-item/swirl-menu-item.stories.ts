5ef9d597c133c7700faed93f3da63ca361fa798c
Ashley <ashley.bekemeier@flipapp.de>
2025-02-04 14:12:45 +0100

    add avatar slot to menu items and action items

--- packages/swirl-components/src/components/swirl-menu-item/swirl-menu-item.stories.ts
+++ packages/swirl-components/src/components/swirl-menu-item/swirl-menu-item.stories.ts
@@ -15,6 +15,15 @@ export default {
 const Template = (args) => {
   const element = generateStoryElement("swirl-menu-item", args);
 
+  element.innerHTML = `
+    <swirl-avatar
+      slot="avatar"
+      label="Jane Doe"
+      size="l"
+      src="https://api.dicebear.com/7.x/bottts-neutral/svg?size=144&seed=a"></swirl-avatar>
+    <swirl-switch checked hide-label label="Test" slot="suffix"></swirl-switch>
+  `;
+
   return element;
 };
 
