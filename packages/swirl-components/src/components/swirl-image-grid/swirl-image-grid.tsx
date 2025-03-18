cea17a587878b2cef8cc00085d9f46be6cfb9c4a
Ashley <59823136+AshleyBekemeier@users.noreply.github.com>
2025-03-18 10:23:47 +0100

    use aspect ratio when given (#991)
    Co-authored-by: Ashley <ashley.bekemeier@flipapp.de>

--- packages/swirl-components/src/components/swirl-image-grid/swirl-image-grid.tsx
+++ packages/swirl-components/src/components/swirl-image-grid/swirl-image-grid.tsx
@@ -9,7 +9,7 @@ import classnames from "classnames";
 export class SwirlImageGrid {
   @Element() el: HTMLSwirlImageGridElement;
 
-  @Prop() aspectRatio?: string = "1/1";
+  @Prop() aspectRatio?: string;
   @Prop() label?: string;
 
   @State() items: HTMLSwirlImageGridItemElement[] = [];
@@ -33,9 +33,12 @@ export class SwirlImageGrid {
   };
 
   caculateAspectRatio = () => {
+    if (this.aspectRatio) {
+      return this.aspectRatio;
+    }
     switch (this.items.length) {
       case 1:
-        return this.aspectRatio;
+        return "1";
       case 2:
         return "2/1";
       case 3:
