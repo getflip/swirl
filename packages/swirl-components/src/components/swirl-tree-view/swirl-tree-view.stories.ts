174a85e9cacd6b8cd69fafc7f029406ba583ba4b
Philipp Siekmann <philipp.siekmann@getflip.com>
2025-03-27 12:36:40 +0100

    chore(components): clean up story

--- packages/swirl-components/src/components/swirl-tree-view/swirl-tree-view.stories.ts
+++ packages/swirl-components/src/components/swirl-tree-view/swirl-tree-view.stories.ts
@@ -21,7 +21,7 @@ const Template = (args) => {
   element.innerHTML = `
     <swirl-tree-view-item href="#" icon="🪁" item-id="item-1" label="Item 1">
       <swirl-tree-view-item href="#" icon="🍄" item-id="item-1-1" label="Item 1.1"></swirl-tree-view-item>
-      <swirl-tree-view-item href="#" icon="🌎" item-id="item-1-2" label="Item 1.2 Item 1.2 Item 1.2 Item 1.2 Item 1.2 Item 1.2 Item 1.2 Item 1.2 Item 1.2 Item 1.2 Item 1.2 Item 1.2 Item 1.2 Item 1.2 Item 1.2 Item 1.2 ">
+      <swirl-tree-view-item href="#" icon="🌎" item-id="item-1-2" label="Item 1.2">
         <swirl-tree-view-item href="#" icon="❄️" item-id="item-1-2-1" label="Item 1.2.1"></swirl-tree-view-item>
         <swirl-tree-view-item href="#" icon="🌭" item-id="item-1-2-2" label="Item 1.2.2">
           <swirl-tag bordered item-id="item-2" label="Draft" size="s" slot="tags"></swirl-tag>
