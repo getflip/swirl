51c573cdb45780619241fc81278e684b8de96484
Philipp <philipp.siekmann@flipapp.de>
2025-08-22 11:04:35 +0200

    feat(components): add optional label prop to SwirlAvatarGroup (#1193)

--- packages/swirl-components/src/components/swirl-avatar-group/swirl-avatar-group.spec.tsx
+++ packages/swirl-components/src/components/swirl-avatar-group/swirl-avatar-group.spec.tsx
@@ -7,7 +7,7 @@ describe("swirl-avatar-group", () => {
     const page = await newSpecPage({
       components: [SwirlAvatarGroup],
       html: `
-        <swirl-avatar-group badge="<swirl-badge aria-label=&quot;3 new messages&quot; label=&quot;3&quot;></swirl-badge>">
+        <swirl-avatar-group badge="<swirl-badge aria-label=&quot;3 new messages&quot; label=&quot;3&quot;></swirl-badge>" label="Label">
           <swirl-avatar label="Jane Doe" src="https://api.dicebear.com/7.x/bottts-neutral/svg?size=144&seed=a" size="s"></swirl-avatar>
           <swirl-avatar label="John Doe" src="https://api.dicebear.com/7.x/bottts-neutral/svg?size=144&seed=b" size="s"></swirl-avatar>
         </swirl-avatar-group>
@@ -15,9 +15,9 @@ describe("swirl-avatar-group", () => {
     });
 
     expect(page.root).toEqualHtml(`
-      <swirl-avatar-group badge="<swirl-badge aria-label=&quot;3 new messages&quot; label=&quot;3&quot;></swirl-badge>">
+      <swirl-avatar-group badge="<swirl-badge aria-label=&quot;3 new messages&quot; label=&quot;3&quot;></swirl-badge>" label="Label">
         <mock:shadow-root>
-          <div class="avatar-group avatar-group--diagonal-stack avatar-group--has-badge" role="group">
+          <div aria-label="Label" class="avatar-group avatar-group--diagonal-stack avatar-group--has-badge" role="group">
             <slot></slot>
             <span class="avatar-group__badge">
               <swirl-badge aria-label="3 new messages" label="3" size="s"></swirl-badge>
