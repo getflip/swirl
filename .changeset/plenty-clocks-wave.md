acb770af23dc6703d462d675c8d45eca079b009b
Philipp <philipp.siekmann@flipapp.de>
2026-01-09 12:38:48 +0100

    Fix aria attributes for popover trigger with hover (#1377)

---
+++ .changeset/plenty-clocks-wave.md
@@ -0,0 +1,8 @@
+---
+"@getflip/swirl-components": patch
+"@getflip/swirl-components-angular": patch
+"@getflip/swirl-components-react": patch
+---
+
+Don't set aria-expanded for swirl-popover-trigger if related popover is
+triggered on hover
--- packages/swirl-components/src/components/swirl-popover-trigger/swirl-popover-trigger.spec.tsx
+++ packages/swirl-components/src/components/swirl-popover-trigger/swirl-popover-trigger.spec.tsx
@@ -34,4 +34,29 @@ describe("swirl-popover-trigger", () => {
       </swirl-popover-trigger>
     `);
   });
+
+  it("renders its trigger element and adds aria attributes when triggered via hover", async () => {
+    const page = await newSpecPage({
+      components: [SwirlPopoverTrigger, SwirlButton],
+      html: `
+        <swirl-popover id="popover"></swirl-popover>
+        <swirl-popover-trigger swirl-popover="popover" trigger-on-hover="true">
+          <swirl-button label="trigger"></swirl-button>
+        </swirl-popover-trigger>
+      `,
+    });
+
+    expect(page.root).toEqualHtml(`
+      <swirl-popover-trigger swirl-popover="popover" trigger-on-hover="true">
+        <swirl-button label="trigger" swirl-aria-haspopup="dialog">
+          <button aria-haspopup="dialog" class="button button--icon-position-start button--intent-default button--size-m button--text-align-center button--variant-ghost" type="button">
+            <span class="button__icon"></span>
+            <span class="button__label">
+              trigger
+            </span>
+          </button>
+        </swirl-button>
+      </swirl-popover-trigger>
+    `);
+  });
 });
