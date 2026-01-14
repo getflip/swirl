ae576b62f8c17fda6dfad7d97687fc39bc5ff589
Paulo Bernardes <joseabernardes@gmail.com>
2026-01-14 14:39:18 +0000

    Migrate swirl-popover and swirl-modal to native popover and dialog

--- packages/swirl-components/typings.d.ts
+++ packages/swirl-components/typings.d.ts
@@ -1,1 +1,13 @@
 declare module "balance-text";
+
+/**
+ * Native Popover API types
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Popover_API
+ */
+
+interface HTMLElement {
+  popover: "auto" | "manual" | null;
+  showPopover(): void;
+  hidePopover(): void;
+  togglePopover(force?: boolean): boolean;
+}
