103a65f88851c7130e6f7137838486893b6d65f3
Paulo Bernardes <joseabernardes@gmail.com>
2026-01-20 13:55:13 +0000

    refactor(components): Use popover to render toasts in top-layer in swirl-toast-provider

--- packages/swirl-components/src/components/swirl-toast-provider/swirl-toast-provider.tsx
+++ packages/swirl-components/src/components/swirl-toast-provider/swirl-toast-provider.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Host, Method, Prop, State } from "@stencil/core";
+import { Component, h, Host, Method, Prop, State, Watch } from "@stencil/core";
 import { SwirlToastCustomEvent } from "../../components";
 import { SwirlToastIntent } from "../swirl-toast/swirl-toast";
 
@@ -19,12 +19,16 @@ export type SwirlToastMessage = SwirlToastConfig & {
 
 @Component({
   shadow: true,
+  styleUrl: "swirl-toast-provider.css",
   tag: "swirl-toast-provider",
 })
 export class SwirlToastProvider {
   @Prop() globalDuration?: number;
 
   @State() private toasts: SwirlToastMessage[] = [];
+
+  private popoverEl: HTMLElement;
+  private slotEl: HTMLSlotElement;
 
   /**
    * Clear all toasts
@@ -69,16 +73,40 @@ export class SwirlToastProvider {
     return newToastWithId;
   }
 
+  @Watch("toasts")
+  onToastsChange() {
+    this.togglePopover();
+  }
+
   onDismiss = (event: SwirlToastCustomEvent<string>) => {
     this.toasts = [...this.toasts].filter(
       (toast) => toast.toastId !== event.detail
     );
   };
 
+  togglePopover = () => {
+    const internalToasts = this.toasts.length;
+    const slottedToasts = this.slotEl.assignedElements().length;
+
+    if (internalToasts + slottedToasts > 0) {
+      this.popoverEl.hidePopover();
+      this.popoverEl.showPopover();
+    } else {
+      this.popoverEl.hidePopover();
+    }
+  };
+
   render() {
     return (
       <Host role="status">
-        <swirl-stack spacing="12">
+        <swirl-stack
+          class="toast-provider__stack"
+          part="toast-provider__stack"
+          popover="manual"
+          spacing="12"
+          align="center"
+          ref={(el) => (this.popoverEl = el)}
+        >
           {this.toasts.map((toast) => {
             const props = { ...toast, content: undefined };
 
@@ -92,6 +120,10 @@ export class SwirlToastProvider {
               </swirl-toast>
             );
           })}
+          <slot
+            onSlotchange={this.togglePopover}
+            ref={(el: HTMLSlotElement) => (this.slotEl = el)}
+          ></slot>
         </swirl-stack>
       </Host>
     );
