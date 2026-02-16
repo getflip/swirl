15e25618b1e544ed3590cd4fa1c23bd72076ecdd
Nuno Faria <2145649+nunofaria11@users.noreply.github.com>
2026-02-16 13:00:37 +0000

    feat: apply icon wrapper contrast colours (#1440)

---
+++ .changeset/new-colts-explode.md
@@ -0,0 +1,7 @@
+---
+"@getflip/swirl-components-angular": minor
+"@getflip/swirl-components-react": minor
+"@getflip/swirl-icons": minor
+---
+
+Apply appropriate contrast text color to wrapped icons
--- packages/swirl-components/src/components/swirl-icon/swirl-icon.spec.tsx
+++ packages/swirl-components/src/components/swirl-icon/swirl-icon.spec.tsx
@@ -51,4 +51,28 @@ describe("swirl-icon", () => {
       wrapper?.classList.contains("icon-wrapper--background-color-blueberry")
     ).toBe(true);
   });
+
+  it("does not pass color to child icon when only wrapperColor is set", async () => {
+    const page = await newSpecPage({
+      components: [SwirlIcon, SwirlIconEdit],
+      html: `<swirl-icon glyph="edit" wrapper-color="blueberry"></swirl-icon>`,
+    });
+
+    const wrapper = page.root.shadowRoot.querySelector(".icon-wrapper");
+    const icon = wrapper?.querySelector("swirl-icon-edit");
+    expect(icon).toBeTruthy();
+    expect(icon?.getAttribute("color")).toBeNull();
+  });
+
+  it("passes explicit color to child icon even when wrapperColor is set", async () => {
+    const page = await newSpecPage({
+      components: [SwirlIcon, SwirlIconEdit],
+      html: `<swirl-icon glyph="edit" wrapper-color="blueberry" color="critical"></swirl-icon>`,
+    });
+
+    const wrapper = page.root.shadowRoot.querySelector(".icon-wrapper");
+    const icon = wrapper?.querySelector("swirl-icon-edit") as any;
+    expect(icon).toBeTruthy();
+    expect(icon?.color).toBe("critical");
+  });
 });
