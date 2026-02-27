9c8e9468190271061f280d685cfb4fc7936aae2b
Philipp Siekmann <philipp.siekmann@getflip.com>
2026-02-27 09:01:08 +0100

    feat(storybook): add custom panel visibility logic for component stories

--- packages/swirl-components/.storybook/manager.js
+++ packages/swirl-components/.storybook/manager.js
@@ -2,6 +2,15 @@ import { addons } from "storybook/manager-api";
 import swirlTheme from "./theme";
 
 addons.setConfig({
+  layoutCustomisations: {
+    showPanel(state, defaultValue) {
+      if (!state.storyId?.startsWith("components-")) {
+        return false;
+      }
+
+      return defaultValue;
+    },
+  },
   panelPosition: "bottom",
   theme: swirlTheme,
 });
